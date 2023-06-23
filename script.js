9d1ecc0b176662da40be50e6cc1925d6dc2f2310
Nixuhs <shu.xin.lin@hs-furtwangen.de>
2023-06-23 17:45:16 +0200

    new

---
+++ script.js
@@ -0,0 +1,134 @@
+/*Product Slider*/ 
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    });
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    });
+
+    item.addEventListener('scroll', () => {
+        let scrollLeft = item.scrollLeft;
+        let maxScrollLeft = item.scrollWidth - item.clientWidth;
+
+        if (scrollLeft === 0) {
+            preBtn[i].style.display = 'none';
+        } else {
+            preBtn[i].style.display = 'block';
+        }
+
+        if (scrollLeft === maxScrollLeft) {
+            nxtBtn[i].style.display = 'none';
+        } else {
+            nxtBtn[i].style.display = 'block';
+        }
+    });
+});
+
+
+
+/*Carousel*/
+
+const carouselContainer = document.querySelector('.carousel-container');
+const button1 = document.querySelector('.slide-button-1');
+const button2 = document.querySelector('.slide-button-2');
+const button3 = document.querySelector('.slide-button-3');
+const slides = document.querySelectorAll('.slide'); 
+const slideWidth = carouselContainer.clientWidth; 
+let currentPosition = 0;
+let timer; 
+
+function showSlide(index) {
+  currentPosition = -slideWidth * index;
+  carouselContainer.style.transform = `translateX(${currentPosition}px)`;
+  updateButtonColor(index);
+}
+
+function updateButtonColor(index) {
+
+  button1.classList.remove('active');
+  button2.classList.remove('active');
+  button3.classList.remove('active');
+
+
+  if (index === 0) {
+    button1.classList.add('active');
+  } else if (index === 1) {
+    button2.classList.add('active');
+  } else if (index === 2) {
+    button3.classList.add('active');
+  }
+}
+
+window.addEventListener('load', () => {
+  updateButtonColor(0);
+});
+
+function startTimer() {
+  timer = setInterval(() => {
+    const currentIndex = Math.abs(currentPosition / slideWidth);
+    const nextIndex = (currentIndex + 1) % slides.length;
+    showSlide(nextIndex);
+  }, 3000); 
+}
+
+function stopTimer() {
+  clearInterval(timer);
+}
+
+button1.addEventListener('click', () => {
+  showSlide(0);
+});
+
+button2.addEventListener('click', () => {
+  showSlide(1);
+});
+
+button3.addEventListener('click', () => {
+  showSlide(2);
+});
+
+startTimer(); 
+
+carouselContainer.addEventListener('mouseenter', stopTimer);
+
+carouselContainer.addEventListener('mouseleave', startTimer);
+
+
+
+
+/*Farb-Button*/
+
+function showImage(imageNumber) {
+  var images = document.getElementsByClassName('image');
+  for (var i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+
+  var selectedImage = document.getElementById('image' + imageNumber);
+  selectedImage.style.display = 'block';
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const menuIcon = document.querySelector(".menu-icon");
+  const menu = document.querySelector(".menu");
+
+  menuIcon.addEventListener("click", function() {
+    menu.classList.toggle("active");
+    menuIcon.classList.toggle("open");
+  });
+
+  var firstButton = document.querySelector(".color-button-1");
+  firstButton.focus({ focusVisible: true });
+});
